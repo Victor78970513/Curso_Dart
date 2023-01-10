6231cd8bc6d45f20db4f6b199edc11292a4d52ae
Victor78970513 <yer29.chok@gmail.com>
2023-01-10 15:23:21 -0400

    Task-1 translation to spanish

--- start/bin/start.dart
+++ start/bin/start.dart
@@ -1,3 +1,10 @@
 void main(List<String> arguments) {
+  int resultado;
   print('Hello world!');
+  resultado = multiplicacion(5, 5);
+  print(resultado);
+}
+
+int multiplicacion(int a, int b) {
+  return a * b;
 }
